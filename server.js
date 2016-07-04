e8918c807538b7c4aa16753595267f7407db5f8a
bharding2 <b.harding@gmail.com>
2016-07-04 00:37:14 -0700

    authenticat plus consolidation

--- server.js
+++ server.js
@@ -1,18 +1,39 @@
 if (!process.env.APP_SECRET) throw new Error('you need an APP_SECRET env variable');
 
 const express = require('express');
+const Router = require('express').Router;
+const bodyParser = require('body-parser').json();
+const handleErr = require(__dirname + '/lib/handle_err');
 const mongoose = require('mongoose');
+const Authenticat = require('authenticat');
 
 const app = express();
 const PORT = process.env.PORT || 5555;
 
-const authRouter = require(__dirname + '/routes/auth_router');
-// const recipeRouter = require(__dirname + '/routes/recipe_router');
+const connection = mongoose.createConnection(process.env.MONGODB_URI ||
+  'mongodb://localhost/ccbrecipe_DB');
+const authenticat = new Authenticat(connection);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/ccbrecipe_DB');
+var recipeSchema = new mongoose.Schema({
+  name: String,
+  creatorId: String
+});
 
-app.use('/api', authRouter);
-// app.use('/api', recipeRouter);
+var Recipe = connection.model('Recipe', recipeSchema);
+var recipeRouter = Router();
+
+app.use('/api', authenticat.router);
+app.use('/api', recipeRouter);
+
+recipeRouter.post('/recipes', authenticat.tokenAuth, bodyParser, (req, res) => {
+  var newRecipe = new Recipe(req.body);
+  newRecipe.creatorId = req.user._id;
+
+  newRecipe.save((err, data) => {
+    if (err) return handleErr(err, res);
+    res.status(200).json(data);
+  });
+});
 
 app.use(express.static(__dirname + '/build'));
 
