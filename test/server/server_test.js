aff7f0d7bc26f323f958b56daeec7835c844c8cf
bharding2 <b.harding@gmail.com>
2016-11-12 12:10:16 -0800

    recipe POST route tests

--- test/server/server_test.js
+++ test/server/server_test.js
@@ -20,9 +20,59 @@ describe('ccbrecipe server', () => {
   });
 
   describe('Recipe methods', () => {
-    it('should pass temporarily', (done) => {
-      expect(true).to.eql(true);
-      done();
+    var userToken = '';
+    var fakeToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InRlc3QxIiwiaWF0IjoxNDc4OTc5MTc4fQ.JB5gC7TLxMoTvzXP8oKC50Oi6YQJ4R6R9YWn5V_fB4w';
+
+    before((done) => {
+      request('localhost:' + port)
+        .post('/api/signup')
+        .send({ username: 'test', password: 'pass' })
+        .end((err, res) => {
+          if (err) return console.log(err);
+          userToken = res.body.token;
+          done();
+        });
+    });
+
+    describe('POST method', () => {
+      it('should POST a new recipe', (done) => {
+        request('localhost:' + port)
+          .post('/api/recipes')
+          .set('token', userToken)
+          .send({ name: 'delicious ants' })
+          .end((err, res) => {
+            expect(err).to.eql(null);
+            expect(res.status).to.eql(200);
+            expect(res.body.name).to.eql('delicious ants');
+            done();
+          });
+      });
+
+      it('should error without authorized user', (done) => {
+        request('localhost:' + port)
+          .post('/api/recipes')
+          .set('token', fakeToken)
+          .send({ name: 'delicious ants' })
+          .end((err, res) => {
+            if (err) console.log(err.message);
+            expect(res.status).to.eql(401);
+            expect(res.body.msg).to.eql('Invalid Authentication');
+            done();
+          });
+      });
+
+      it('should error without valid data', (done) => {
+        request('localhost:' + port)
+          .post('/api/recipes')
+          .set('token', userToken)
+          .send({ wrong: 'data' })
+          .end((err, res) => {
+            if (err) console.log(err.message);
+            expect(res.status).to.eql(401);
+            expect(res.body.msg).to.eql('error saving recipe');
+            done();
+          });
+      });
     });
   });
 });
