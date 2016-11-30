452bffdc9172aebbaf246113d7c01aaedf446e98
bharding2 <b.harding@gmail.com>
2016-11-30 14:08:51 -0800

    recipe put admin tests

--- test/server/server_test.js
+++ test/server/server_test.js
@@ -57,7 +57,6 @@ describe('ccbrecipe server', () => {
 
               User.findOneAndUpdate({ _id: res.body.id }, { admin: true }, (err) => {
                 if (err) console.log(err);
-                console.log('admins ahoy');
                 done();
               });
             });
@@ -299,6 +298,19 @@ describe('ccbrecipe server', () => {
           });
       });
 
+      it('should admin PUT an update to a recipe', (done) => {
+        request('localhost:' + port)
+          .put('/api/recipes/' + newRecipe._id)
+          .set('token', adminToken)
+          .send({ name: 'delicious admins' })
+          .end((err, res) => {
+            expect(err).to.eql(null);
+            expect(res.status).to.eql(200);
+            expect(res.body.msg).to.eql('recipe updated by admin');
+            done();
+          });
+      });
+
       it('should not put without authorized user', (done) => {
         request('localhost:' + port)
           .put('/api/recipes/' + newRecipe._id)
@@ -325,11 +337,37 @@ describe('ccbrecipe server', () => {
           });
       });
 
+      it('should not admin put without valid data', (done) => {
+        request('localhost:' + port)
+          .put('/api/recipes/' + newRecipe._id)
+          .set('token', adminToken)
+          .send({ wrong: 'data' })
+          .end((err, res) => {
+            if (err) console.log(err.message);
+            expect(res.status).to.eql(401);
+            expect(res.body.msg).to.eql('error updating recipe');
+            done();
+          });
+      });
+
       it('should not put without a valid recipe id', (done) => {
         request('localhost:' + port)
           .put('/api/recipes/fakerecipeid')
           .set('token', userToken)
           .send({ name: 'delicious ants' })
+          .end((err, res) => {
+            if (err) console.log(err.message);
+            expect(res.status).to.eql(404);
+            expect(res.body.msg).to.eql('recipe not found');
+            done();
+          });
+      });
+
+      it('should not admin put without a valid recipe id', (done) => {
+        request('localhost:' + port)
+          .put('/api/recipes/fakerecipeid')
+          .set('token', adminToken)
+          .send({ name: 'delicious admins' })
           .end((err, res) => {
             if (err) console.log(err.message);
             expect(res.status).to.eql(404);
