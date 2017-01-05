0c3f3455dceff32d26d2bd6d5b857953a9f30f8d
bharding2 <b.harding@gmail.com>
2017-01-05 06:12:26 -0800

    simple route with prop

--- app/js/entry.js
+++ app/js/entry.js
@@ -4,6 +4,8 @@ const ReactDOM = require('react-dom');
 const Router = require('react-router').Router;
 const Route = require('react-router').Route;
 const Link = require('react-router').Link;
+const browserHistory = require('react-router').browserHistory;
+const withRouter = require('react-router').withRouter;
 
 var apiUrl = process.env.API_URL;
 
@@ -11,13 +13,15 @@ const RecipeBox = React.createClass({
   render: function() {
     return (
       <div>
-        <p>{ this.props.url }</p>
+        <p>{ this.props.route.url }</p>
       </div>
     );
   }
 });
 
-ReactDOM.render(
-  <RecipeBox url={ apiUrl } />,
-  document.getElementById('ccbrecipe_app')
-);
+ReactDOM.render((
+  <Router history={ browserHistory }>
+    <Route path="/" url={ apiUrl } component={ RecipeBox }>
+    </Route>
+  </Router>
+), document.getElementById('ccbrecipe_app'));
