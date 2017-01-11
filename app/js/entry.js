07a4ddf63dfa9fa0905fa1ac7d88d8ab3d108e73
bharding2 <b.harding@gmail.com>
2017-01-11 14:44:15 -0800

    admin routes closes 63

--- app/js/entry.js
+++ app/js/entry.js
@@ -33,6 +33,18 @@ ReactDOM.render((
       <Route path="recipe" component={ Recipes }>
         <Route path=":recipeId" component={ Recipe }/>
       </Route>
+      <Route path="admin" component={ AdminBox }>
+        <Route path="edit_recipe" component={ EditRecipes }>
+          <Route path=":recipeId" component={ EditRecipe }/>
+        </Route>
+        <Route path="users" recipesBatch= { current } component={ Users }>
+          <Route path="new" component={ NewUser }/>
+          <Route path=":userId" component={ User }/>
+          <Route path="edit_user" component={ EditUsers }>
+            <Route path=":userId" component={ EditUser }/>
+          </Route>
+        </Route>
+      </Route>
     </Route>
   </Router>
 ), document.getElementById('ccbrecipe_app'));
